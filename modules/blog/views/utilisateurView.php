e6f8a472b038f371814f346994a112f09d2adc5a
DAUBREGE-Simon-2326036aa <simon.daubrege@etu.univ-amu.fr>
2024-10-11 10:20:36 +0200

    css de la page de connexion avancé

--- modules/blog/views/utilisateurView.php
+++ modules/blog/views/utilisateurView.php
@@ -18,24 +18,26 @@ class utilisateurView
         $navebar->afficher();
   ?>
 
+        <link rel="stylesheet" href="/GestionSalleDeSportSae/assets/styles/login.css">
+
         <div class="login-container">
-            <h2>Connexion</h2>
-            <form method="POST" action="connexion" class="login-form">
-                <div class="input-group">
+            <div class="connexion_box">
+                <h2>Connectez-vous à votre espace</h2>
+                <form method="POST" action="connexion" class="login-form">
                     <label for="email">Adresse email</label>
-                    <input type="email" id="email" name="mail" required>
-                </div>
-                <div class="input-group">
+                    <div class="input-group">
+                        <input type="email" id="email" name="mail"  placeholder="Mon email" required>
+                    </div>
                     <label for="password">Mot de passe</label>
-                    <input type="password" id="password" name="mdp" required>
-                </div>
-                <button type="submit" name="inscription" id="inscription" class="login-btn">Se connecter</button>
-            </form> <?php echo $message?>
+                    <div class="input-group">
+                        <input type="password" id="password" name="mdp"  placeholder="Mon mot de passe" required>
+                    </div>
+                    <button type="submit" name="inscription" id="inscription" class="login-btn">Se connecter</button>
+                    <button type="new_acc" name="creer-compte" id="creer-compte" class="create-btn">Créer un compte</button>
+                </form> <?php echo $message?>
+            </div>
         </div>
         <?php include 'footer.php';
-
-
-
 }
 
 }
