335d7903be3ee0f93df5fc31c445cfe0506f0def
BARBERO-Tom-2326014b <tom.barbero@etu.univ-amu.fr>
2024-12-09 09:49:35 +0100

    verif abonnement

--- modules/blog/models/verifModel.php
+++ modules/blog/models/verifModel.php
@@ -1,4 +1,42 @@
 <?php
-    mail("tom.om.barbero@gmail.com","test","coucoucjcoej kvn^knv");
-    ?>
+
+    namespace blog\models;
+use PDO;
+use PDOException;
+
+require_once "modules/blog/models/bdModel.php";
+
+class verifModel
+{
+    private $connexionBD;
+
+    public function __construct()
+    {
+        try {
+            $this->connexionBD = new bdModel();
+        } catch (PDOException $e) {
+            die("Erreur de connexion à la base de données : " . $e->getMessage());
+        }
+    }
+
+    public function requete(){
+        $requete = $this->connexionBD->pdo->prepare(" SELECT idUtilisateur FROM abonnement WHERE dateExp < DATE_ADD(CURDATE(), INTERVAL 1 MONTH)");
+        $requete->execute();
+        $donnees = $requete->fetch();
+        foreach ($donnees as $id) {
+            $requete2 = $this->connexionBD->pdo->prepare(" Select EMail from utilisateur where idUtilisateur =:idUtil");
+            $requete2->bindParam(":idUtil", $id["idUtilisateur"]);
+            $requete2->execute();
+            $donnees2 = $requete2->fetch();
+            foreach ($donnees2 as $mail){
+                mail($mail["EMail"], "Votre abonnement expire bientot", "Bonjour vôtre abonnement expire dans un mois ! ");
+            }
+
+        }
+        $this->construct = new verifModel();
+        $this->construct->requete();
+
+    }
+}
+?>
 
