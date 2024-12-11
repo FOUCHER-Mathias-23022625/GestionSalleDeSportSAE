f7344644d8e0e80a7b76bce5fc76439bffa48f26
BARBERO-Tom-2326014b <tom.barbero@etu.univ-amu.fr>
2024-12-11 19:31:26 +0100

    page compte

--- assets/scripts/compte.js
+++ assets/scripts/compte.js
@@ -13,3 +13,13 @@ const Prenom = document.getElementById('PrenomU');
 const modifPrenom = document.getElementById('prenomModif');
 modifPrenom.addEventListener('click', function() {
     Prenom.disabled = false; });// Action pour la connexion
+
+    const Nom = document.getElementById('NomU');
+    const modifNom = document.getElementById('nomModif');
+    modifNom.addEventListener('click', function() {
+        Nom.disabled = false; });// Action pour la connexion
+
+    const Email = document.getElementById('Email');
+    const modifEmail = document.getElementById('mailModif');
+    modifEmail.addEventListener('click', function() {
+        Email.disabled = false; });// Action pour la connexion
