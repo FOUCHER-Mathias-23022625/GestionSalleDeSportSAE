bded4f9ba976b203852d3d0543dc3ecd2e6ec492
BARBERO-Tom-2326014b <tom.barbero@etu.univ-amu.fr>
2024-12-11 14:21:35 +0100

    renvoi sur la page d'accueil apres connexion

--- modules/blog/controllers/interfaceAdminController.php
+++ modules/blog/controllers/interfaceAdminController.php
@@ -1,15 +1,19 @@
 <?php
-
+namespace blog\controllers;
 namespace controllers;
+
 
 use blog\views\interfaceAdminView;
 use blog\models\interfaceAdminModel;
 use blog\models\compteModel;
+use Index;
+use blog\views\Layout;
 
-require_once  "./index.php";
+
 require_once "modules/blog/views/interfaceAdminView.php";
 require_once "modules/blog/models/interfaceAdminModel.php";
 require_once "modules/blog/models/compteModel.php";
+require_once "modules/blog/views/Layout.php";
 
 class interfaceAdminController
 {
