8350fa46df2d00555991d65d8e6c20d5f0b93292
Foucher Mathias <mathias.foucher@etu.univ-amu.fr>
2024-12-15 12:55:03 +0100

    changements sur reservations

--- modules/blog/controllers/reservationTerrainController.php
+++ modules/blog/controllers/reservationTerrainController.php
@@ -37,8 +37,8 @@ class  reservationTerrainController
         $request_res = $this->reservationTerrainModele->getReservationTerrain($selected_date, $selected_sport, 1);
         $request_resBis = $this->reservationTerrainModele->getReservationTerrain($selected_date, $selected_sport, 2);
         if ($selected_sport && $selected_date): ?>
-            <h2><?php echo htmlspecialchars($selected_sport); ?></h2>
-            <p><?php echo htmlspecialchars($selected_date); ?></p>
+            <h2 class="titreSportSelec"><?php echo htmlspecialchars($selected_sport); ?></h2>
+            <p class="dateSelectionné"><?php echo htmlspecialchars($selected_date); ?></p>
         <?php
             // Liste complète des créneaux horaires (par exemple de 8h à 20h)
             $full_time_slots = range(8, 20); // Vous pouvez ajuster cette plage selon vos besoins
@@ -63,31 +63,31 @@ class  reservationTerrainController
             ?>
             <section class="Reservation">
                 <div class="colonne1">
-                <h3>Terrain 1</h3>
-                <div class="cardRes">
-                    <?php if (!empty($available_time_slots)): ?>                        <?php foreach ($available_time_slots as $time): ?>
-                            <button class="time-slot" onclick="openModal('<?php echo htmlspecialchars($time); ?>', 1)">
-                                <?php echo htmlspecialchars($time); ?>:00 H
-                            </button>
-                        <?php endforeach; ?>
-                    <?php else: ?>
-                        <p>Aucun créneau disponible pour <?php echo htmlspecialchars($selected_sport); ?> sur Terrain 1 ce jour-là.</p>
-                    <?php endif; ?>
-                </div>
+                    <h3>Terrain 1</h3>
+                    <div class="cardRes">
+                        <?php if (!empty($available_time_slots)): ?>                        <?php foreach ($available_time_slots as $time): ?>
+                                <button class="time-slot" onclick="openModal('<?php echo htmlspecialchars($time); ?>', 1)">
+                                    <?php echo htmlspecialchars($time); ?>:00 H
+                                </button>
+                            <?php endforeach; ?>
+                        <?php else: ?>
+                            <p>Aucun créneau disponible pour <?php echo htmlspecialchars($selected_sport); ?> sur Terrain 1 ce jour-là.</p>
+                        <?php endif; ?>
+                    </div>
                 </div>
                 <div class="colonne1">
-                <h3>Terrain 2</h3>
-                <div class="cardRes">
-                    <?php if (!empty($available_time_slotsBis)): ?>
-                        <?php foreach ($available_time_slotsBis as $time2): ?>
-                            <button class="time-slot" onclick="openModal('<?php echo htmlspecialchars($time2); ?>', 2)">
-                                <?php echo htmlspecialchars($time2); ?>:00 H
-                            </button>
-                        <?php endforeach; ?>
-                    <?php else: ?>
-                        <p>Aucun créneau disponible pour <?php echo htmlspecialchars($selected_sport); ?> sur Terrain 2 ce jour-là.</p>
-                    <?php endif; ?>
-                </div>
+                    <h3>Terrain 2</h3>
+                    <div class="cardRes">
+                        <?php if (!empty($available_time_slotsBis)): ?>
+                            <?php foreach ($available_time_slotsBis as $time2): ?>
+                                <button class="time-slot" onclick="openModal('<?php echo htmlspecialchars($time2); ?>', 2)">
+                                    <?php echo htmlspecialchars($time2); ?>:00 H
+                                </button>
+                            <?php endforeach; ?>
+                        <?php else: ?>
+                            <p>Aucun créneau disponible pour <?php echo htmlspecialchars($selected_sport); ?> sur Terrain 2 ce jour-là.</p>
+                        <?php endif; ?>
+                    </div>
                 </div>
             </section>
         <?php
