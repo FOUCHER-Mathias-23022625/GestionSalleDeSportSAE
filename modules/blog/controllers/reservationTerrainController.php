214828f7c11ff2b49d7d8072d20da3c027912aea
BOUCHAMI-Enzo-2326025aa <enzo.bouchami@etu.univ-amu.fr>
2025-01-10 11:39:25 +0100

    probleme de code resulo ( du html dans le controller qui est mtn dans la vue )

--- modules/blog/controllers/reservationTerrainController.php
+++ modules/blog/controllers/reservationTerrainController.php
@@ -41,14 +41,10 @@ class  reservationTerrainController
         $view->afficher($selected_date,$selected_sport,$reservation_status);
     }
 
-    public function afficheRes($selected_date, $selected_sport): void
+    public function afficheRes($selected_date, $selected_sport): array
     {
         $request_res = $this->reservationTerrainModele->getReservationTerrain($selected_date, $selected_sport, 1);
         $request_resBis = $this->reservationTerrainModele->getReservationTerrain($selected_date, $selected_sport, 2);
-        if ($selected_sport && $selected_date): ?>
-            <h2 class="titreSportSelec"><?php echo htmlspecialchars($selected_sport); ?></h2>
-            <p class="dateSelectionné"><?php echo htmlspecialchars($selected_date); ?></p>
-        <?php
             // Liste complète des créneaux horaires (par exemple de 8h à 20h)
             $full_time_slots = range(8, 20); // Vous pouvez ajuster cette plage selon vos besoins
 
@@ -69,38 +65,11 @@ class  reservationTerrainController
             // Trouver les créneaux non réservés
             $available_time_slots = array_diff($full_time_slots, $reserved_time_slots);
             $available_time_slotsBis = array_diff($full_time_slots, $reserved_time_slotsBis);
-            ?>
-            <section class="Reservation">
-                <div class="colonne1">
-                    <h3>Terrain 1</h3>
-                    <div class="cardRes">
-                        <?php if (!empty($available_time_slots)): ?>                        <?php foreach ($available_time_slots as $time): ?>
-                                <button class="time-slot" onclick="openModal('<?php echo htmlspecialchars($time); ?>', 1)">
-                                    <?php echo htmlspecialchars($time); ?>:00 H
-                                </button>
-                            <?php endforeach; ?>
-                        <?php else: ?>
-                            <p>Aucun créneau disponible pour <?php echo htmlspecialchars($selected_sport); ?> sur Terrain 1 ce jour-là.</p>
-                        <?php endif; ?>
-                    </div>
-                </div>
-                <div class="colonne1">
-                    <h3>Terrain 2</h3>
-                    <div class="cardRes">
-                        <?php if (!empty($available_time_slotsBis)): ?>
-                            <?php foreach ($available_time_slotsBis as $time2): ?>
-                                <button class="time-slot" onclick="openModal('<?php echo htmlspecialchars($time2); ?>', 2)">
-                                    <?php echo htmlspecialchars($time2); ?>:00 H
-                                </button>
-                            <?php endforeach; ?>
-                        <?php else: ?>
-                            <p>Aucun créneau disponible pour <?php echo htmlspecialchars($selected_sport); ?> sur Terrain 2 ce jour-là.</p>
-                        <?php endif; ?>
-                    </div>
-                </div>
-            </section>
-        <?php
-        endif;
+
+        return [
+            'available_time_slots' => $available_time_slots,
+            'available_time_slotsBis' => $available_time_slotsBis
+        ];
     }
 
 
